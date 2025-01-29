68585721ce75ad7c729000dbf151de3059011e21
Lakedeeper <skry.emir9@gmail.com>
2025-01-29 11:29:21 +0300

    Doktorların cvleri değiştirildi
    ss

--- front-end/src/Components/doctors/Doctors.jsx
+++ front-end/src/Components/doctors/Doctors.jsx
@@ -22,11 +22,8 @@ function Doctors() {
             </div>
             <div className="doktorsP">
               <p>
-                Diş Kozmetiği genelde diş beyazlatma, kaplamalar gibi diş
-                yüzeyindeki diş işleri ile ilgilenme eğilimindedir. Oysa ki
-                estetik daha derinlemesine değerlendirilmeli, diş problemleri
-                düzeltilmeli, implant, kron gibi işlemlerle bir ağız bakımı
-                gerçekleştirilmelidir.
+              Diş Hekimi Hakan Kayhan 2019 yılında Bezmialem Vakıf Üniversitesi Diş Hekimliği Fakültesinden mezun oldu. 3 yıl süreyle çeşitli hastane ve polikliniklerde diş hekimliği yaptı. 2022 yılında Olydent ailesinin kurucuları arasına katıldı. Bu tarihten itibaren Olydent çatısı altında diş hekimliği mesleğini sürdürmektedir. Hakan Kayhan Protez uygulamaları ve Gülüş tasarımı alanlarında aldığı eğitimler ve edindiği
+              tecrübelerle hastalarımıza hakettikleri gülüşü kazanmaları için yardımcı olmaktadır.
               </p>
             </div>
           </div>
@@ -41,11 +38,7 @@ function Doctors() {
             </div>
             <div className="doktorsP">
               <p>
-                Estetik Diş Hekimliği, diş tedavisinde bilim ve sanatın
-                birleştiği yerdir. Aynı zamanda belirli bir tedavi disiplini
-                veya türü olmaktan ziyade estetik, fonksiyonel ve biyolojik
-                açıdan hasta için en iyi sonucu yaratmak amacıyla tüm diş tedavi
-                disiplinlerini kapsar.
+              Diş Hekimi Mert Sakarya, Bezmialem Vakıf Üniversitesi Diş Hekimliği bölümünden mezun olmuştur. 3 yıl kadar kendi muayenesinde diş hekimi olarak çalıştıktan sonra 2022 yılında Olydent ailesinin kuruşlar arasına katılmıştır. Bu tarihten itibaren Olydent çatısı altında diş hekimliği mesleğini sürdürmektedir. Mert Sakarya implant ve protez uygulamaları alanında edindiği bilgi birikimle hastalarımızın yeniden sağlıklı ve mutlu gülüşlere kavuşmaları için yardımcı olmaktadır.
   
               </p>
             </div>
